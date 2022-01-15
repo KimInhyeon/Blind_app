3e8b5d1907675ca23f0c4ed5c2ecb658cf19e869
kwunodong <kwunodong@gmail.com>
2022-01-15 14:25:56 +0900

    年齢（給料）機能Page画面制作中。

---
+++ app/src/main/java/com/ksinfo/blind/annualincome/AnnualIncomeRankCalculatorActivityShowUserRank.java
@@ -0,0 +1,16 @@
+package com.ksinfo.blind.annualincome;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ksinfo.blind.R;
+
+public class AnnualIncomeRankCalculatorActivityShowUserRank extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.annual_income_rank_calculator_show_user_rank);
+    }
+}
