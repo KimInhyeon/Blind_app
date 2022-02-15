b6b176fd9b2d64857c88dda4c6085b6df6515d94
kwunodong <kwunodong@gmail.com>
2022-02-15 20:38:01 +0900

    [年齢登録計算機（AI）]AI（AnnualIncome）のRESファイルアーキテクチャ更新。

--- app/src/main/java/com/ksinfo/tomodomo/model/itf/WorkTypeInterface.java
+++ app/src/main/java/com/ksinfo/tomodomo/model/itf/WorkTypeInterface.java
@@ -1,12 +1,12 @@
-package com.ksinfo.blind.annualincome.api;
+package com.ksinfo.tomodomo.model.itf;
 
-import com.ksinfo.blind.annualincome.vo.CompanyWorkTypeVO;
+import com.ksinfo.tomodomo.model.vo.annualincome.CompanyWorkTypeVO;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import java.util.List;
 
-public interface CompanyWorktypeApi {
+public interface WorkTypeInterface {
     @GET("company/annualIncome/annualIncomeCalculator2")
     Call<List<CompanyWorkTypeVO>> getWorkTypeAll();
 }
