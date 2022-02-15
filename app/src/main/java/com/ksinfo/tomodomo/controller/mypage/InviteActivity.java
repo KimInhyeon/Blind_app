b5d539e02f793ed449e004dc2462306a481ed77c
kwunodong <kwunodong@gmail.com>
2022-02-15 20:56:07 +0900

    [招待する（iv）]iv（Invite）のJAVAファイルアーキテクチャ更新_2。

--- app/src/main/java/com/ksinfo/tomodomo/controller/mypage/InviteActivity.java
+++ app/src/main/java/com/ksinfo/tomodomo/controller/mypage/InviteActivity.java
@@ -11,7 +11,7 @@ public class InviteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mp_invite);
+        setContentView(R.layout.iv_main);
 
     }
 
